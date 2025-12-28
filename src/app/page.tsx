ebac01da335420cad9f7a4707e8f441c34a5bb8b
workingwithali <alirashid2020e@gmail.com>
2025-12-28 10:32:34 +0500

    setup next js

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { ThemeToggle } from "@/components/theme-toggle";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <ThemeToggle />
+    </div>
+  );
+}
---
+++ src/components/theme-toggle.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { useTheme } from 'next-themes';
+import { Button } from '@/components/ui/button';
+import { Moon, Sun } from 'lucide-react';
+import { useEffect, useState } from 'react';
+
+export function ThemeToggle() {
+  const { setTheme, theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+
+  return (
+    <Button
+      variant="ghost"
+      size="icon"
+      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+    >
+      {theme === 'dark' ? <Sun /> : <Moon />}
+    </Button>
+  );
+}
